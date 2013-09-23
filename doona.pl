ec24ed466a60f95314648869e583b179eb94ff76
Eldar Marcussen <wireghoul@justanotherhacker.com>
2013-09-23 17:42:29 -0400

    code cleanup for better readability

--- doona.pl
+++ doona.pl
@@ -9,11 +9,11 @@
 use Getopt::Std;
 use Socket;
 use Config;
+
 #use strict;
 #use warnings;
 my $SOCKET = "";
 my $VERSION = '0.7';
-
 
 $SIG{'INT'} = \&sigHandler;
 $SIG{'TERM'} = \&sigHandler;
@@ -29,14 +29,14 @@ my @formatstrings = ("%s" x 4, "%s%p%x%d", "%s" x 8, "%s" x 15, "%s" x 30, "%.10
 # three ansi overflows, two ansi format strings, two OEM Format Strings
 my @unicodestrings = ("\0x99"x4, "\0x99"x512, "\0x99"x1024, "\0xCD"x10, "\0xCD"x40, "\0xCB"x10, "\0xCB"x40);
 my @largenumbers = (
-    "255", "256", "257", 
-    "65535", "65536", "65537", 
-    "16777215", "16777216", "16777217", 
+    "255", "256", "257",
+    "65535", "65536", "65537",
+    "16777215", "16777216", "16777217",
     "0xffffffff", "2147483647", "2147483648", "2147483649",
     "4294967295", "9223372036854775807", "18446744073709551615",
     "-1", "-268435455", "-20",
     "2.2250738585072011e-308",
-);
+  );
 my @miscstrings = ("/", "\\", "%0xa", " ", "+", "<", ">", "<>", "%", "-", "+", "*", ".", ":", "&", "%u000", "\t", "\r", "\r\n", "\n");
 my $idx = 0;
 my $prevfuzz = '';
@@ -50,10 +50,10 @@ $opt_s = lc($opt_s);                         # convert it to lowercase
 
 # load the specified module
 my $module = undef;
-if ( -f "bedmod/$opt_s.pm"){
-        eval("use bedmod::$opt_s;");
-        $a = "bedmod::$opt_s";
-        $module = new $a;
+if ( -f "bedmod/$opt_s.pm") {
+    eval("use bedmod::$opt_s;");
+    $a = "bedmod::$opt_s";
+    $module = new $a;
 }
 
 &usage unless(defined $module);
@@ -85,6 +85,7 @@ if ($special_cfg{'o'} eq "") { $module->{timeout}='2'; }
 else { $module->{timeout} = $special_cfg{'o'}; }
 
 $module->init(%special_cfg);
+
 # $num_threads = 4; # Wishlist: Run with 4 threads by defaults
 
 # test stuff that might happen during login
@@ -127,86 +128,86 @@ print "* All tests done.\n";
 exit(0);
 
 # this function tests each of the two arrays ( buffer overflow and format string )
-sub testThis(){
+sub testThis() {
     foreach my $log (@login) {
-    my @testArray = @_;
-    my $command;
-    my $socktype;
-    if ( $module->{proto} eq "udp" ){
-        $socktype = SOCK_DGRAM;
-    } else {
-        $socktype = SOCK_STREAM;
-    }
-    $|=1; #Auto flush output for more timely screen updates
-    my $count = 0;
-    my $quit = $module->getQuit;
-    my $total = scalar(@cmdArray);
-    foreach my $cmd (@cmdArray){
-        $count++;
-
-        my $cmd2 = $cmd;
-        $cmd2 =~ s/\n|\r|[\00-\33]//ig;                              # remove \r and \n for nice displaying
-        $cmd2 = substr($cmd2, 0, 30);
-
-        my $log2 = $log;
-        $log2 =~ s/\n|\r|[\00-\33]//ig;
-        $log2 = substr($log2, 0, 10);
-
-        print "\ttesting: $count/$total\t$log2 [$cmd2] ";
-        foreach my $LS (@testArray){
-            print ".";
-            $idx++;
-            if ($special_cfg{'r'} > $idx) { next; }
-            if ($opt_M) { $special_cfg{'M'}--; }
-            if ($special_cfg{'M'} < 0) { print "\nMax requests ($opt_M) completed, index: ". ($idx - 1) ."\n"; exit }
-            $prevfuzz = $command;
-            $command = $cmd;
-            $command =~ s/XAXAX/$LS/ig;                   # prepare the string
-            if ($special_cfg{'d'}) { 
-              print "\nFuzz case: --copy--\n"; 
-            } else {
-              my $iaddr = inet_aton($module->{target})             || die "Unknown host: $module->{target}\n";
-              my $paddr = sockaddr_in($module->{port}, $iaddr)     || die "getprotobyname: $!\n";
-              my $proto = getprotobyname($module->{proto})         || die "getprotobyname: $!\n";
-              socket(SOCKET, PF_INET, $socktype, $proto)           || die "socket: $!\n";
-              my $sockaddr = sockaddr_in($module->{sport}, INADDR_ANY);
-              while ( !bind(SOCKET, $sockaddr) ) {}         # we need to bind for LPD for example
-              connect(SOCKET, $paddr)                             || die "connection attempt failed: $!, during $cmd2 ($idx)\n";
-            }
-
-            # login
-            #foreach my $log (@login){
-                if ( $log ne "" ){
-                  if ($special_cfg{'d'}) {
-                    print "$log";
-                  } else {
-                    send(SOCKET, $log, 0);
-                    sleep(1);                     # some daemons need some time to reply
-                  }
-                }
-            #}
-            if ($special_cfg{'d'}) { print "$command\\r\\n\n--cut--\n"; exit; }
-            send(SOCKET, $command, 0);                    # send the attack and verify that the server is still alive
-            # Is there a possibility to check within connection?
-            if ($module->{vrfy} ne "") {
-                send(SOCKET, $module->{vrfy},0)                  || die "Problem (1) occured with $cmd2 ($idx)\n";
-                my $recvbuf = <SOCKET>                           || die "Problem (2) occured with $cmd2 ($idx)\n";
-                send(SOCKET, $quit, 0);           # close the connection
-                close SOCKET;
-            } else {
-                close SOCKET;
-                $iaddr = inet_aton($module->{target})            || die "Unknown host: $module->{target}\n";
-                $paddr = sockaddr_in($module->{port}, $iaddr)    || die "getprotobyname: $!\n";
-                $proto = getprotobyname($module->{proto})        || die "getprotobyname: $!\n";
-                socket(SOCKET, PF_INET, $socktype, $proto)       || die "socket: $!\n";
-                connect(SOCKET, $paddr)                          || die "Problem (3) occured with $cmd2 ($idx)\n";
-                close SOCKET;
-            }
-
-            sleep($module->{timeout});                                             # some servers would kick us for too fast rogins
+        my @testArray = @_;
+        my $command;
+        my $socktype;
+        if ( $module->{proto} eq "udp" ) {
+            $socktype = SOCK_DGRAM;
+        } else {
+            $socktype = SOCK_STREAM;
         }
-        print " ($idx)\n";
-    }
+        $|=1; #Auto flush output for more timely screen updates
+        my $count = 0;
+        my $quit = $module->getQuit;
+        my $total = scalar(@cmdArray);
+        foreach my $cmd (@cmdArray) {
+            $count++;
+
+            my $cmd2 = $cmd;
+            $cmd2 =~ s/\n|\r|[\00-\33]//ig;                              # remove \r and \n for nice displaying
+            $cmd2 = substr($cmd2, 0, 30);
+
+            my $log2 = $log;
+            $log2 =~ s/\n|\r|[\00-\33]//ig;
+            $log2 = substr($log2, 0, 10);
+
+            print "\ttesting: $count/$total\t$log2 [$cmd2] ";
+            foreach my $LS (@testArray){
+                print ".";
+                $idx++;
+                if ($special_cfg{'r'} > $idx) { next; }
+                if ($opt_M) { $special_cfg{'M'}--; }
+                if ($special_cfg{'M'} < 0) { print "\nMax requests ($opt_M) completed, index: ". ($idx - 1) ."\n"; exit }
+                $prevfuzz = $command;
+                $command = $cmd;
+                $command =~ s/XAXAX/$LS/ig;                   # prepare the string
+                if ($special_cfg{'d'}) {
+                    print "\nFuzz case: --copy--\n";
+                } else {
+                    my $iaddr = inet_aton($module->{target})             || die "Unknown host: $module->{target}\n";
+                    my $paddr = sockaddr_in($module->{port}, $iaddr)     || die "getprotobyname: $!\n";
+                    my $proto = getprotobyname($module->{proto})         || die "getprotobyname: $!\n";
+                    socket(SOCKET, PF_INET, $socktype, $proto)           || die "socket: $!\n";
+                    my $sockaddr = sockaddr_in($module->{sport}, INADDR_ANY);
+                    while ( !bind(SOCKET, $sockaddr) ) {}         # we need to bind for LPD for example
+                    connect(SOCKET, $paddr)                             || die "connection attempt failed: $!, during $cmd2 ($idx)\n";
+                }
+
+                # login
+                if ( $log ne "" ) {
+                    if ($special_cfg{'d'}) {
+                        print "$log";
+                    } else {
+                        send(SOCKET, $log, 0);
+                        sleep(1);                     # some daemons need some time to reply
+                    }
+                }
+
+                #}
+                if ($special_cfg{'d'}) { print "$command\\r\\n\n--cut--\n"; exit; }
+                send(SOCKET, $command, 0);                    # send the attack and verify that the server is still alive
+                # Is there a possibility to check within connection?
+                if ($module->{vrfy} ne "") {
+                    send(SOCKET, $module->{vrfy},0)                  || die "Problem (1) occured with $cmd2 ($idx)\n";
+                    my $recvbuf = <SOCKET>                           || die "Problem (2) occured with $cmd2 ($idx)\n";
+                    send(SOCKET, $quit, 0);           # close the connection
+                    close SOCKET;
+                } else {
+                    close SOCKET;
+                    $iaddr = inet_aton($module->{target})            || die "Unknown host: $module->{target}\n";
+                    $paddr = sockaddr_in($module->{port}, $iaddr)    || die "getprotobyname: $!\n";
+                    $proto = getprotobyname($module->{proto})        || die "getprotobyname: $!\n";
+                    socket(SOCKET, PF_INET, $socktype, $proto)       || die "socket: $!\n";
+                    connect(SOCKET, $paddr)                          || die "Problem (3) occured with $cmd2 ($idx)\n";
+                    close SOCKET;
+                }
+
+                sleep($module->{timeout});                                             # some servers would kick us for too fast rogins
+            }
+            print " ($idx)\n";
+        }
     }
 }
 
@@ -229,14 +230,14 @@ sub usage {
 
 ~;
     if ($opt_h) {
-      $module->usage() if $module;
+        $module->usage() if $module;
     }
     exit(1);
 }
 
 sub sigHandler {
-        print "\n\nSignal INT/TERM/PIPE caught!";
-        print " - current test case index: ($idx)" if $idx;
-        print "\n";
-        exit;
+    print "\n\nSignal INT/TERM/PIPE caught!";
+    print " - current test case index: ($idx)" if $idx;
+    print "\n";
+    exit;
 }
