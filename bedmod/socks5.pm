25fe114e3cc52183b71bd2f6105ee89bd08b306a
Eldar Marcussen <wireghoul@justanotherhacker.com>
2013-11-18 22:57:47 -0500

    Fixed whitespace for socks5 module

--- bedmod/socks5.pm
+++ bedmod/socks5.pm
@@ -7,33 +7,33 @@ use Socket;
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # create a new instance of this object
-sub new{
-	my $this = {};
-	$this->{username}     = undef;
-        $this->{password}     = undef;
-	bless $this;
-	return $this;
+sub new {
+    my $this = {};
+    $this->{username} = undef;
+    $this->{password} = undef;
+    bless $this;
+    return $this;
 }
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # initialise some parameters
-sub init{
+sub init {
     my $this = shift;
-    %special_cfg=@_;
+    %special_cfg = @_;
 
     # Set protocol tcp/udp
-    $this->{proto} = "tcp"; 
+    $this->{proto} = "tcp";
 
     # check for missing args, set target and host
     # every module has to do this
-    if ($special_cfg{'p'} eq "") { $this->{port}='1080'; }
-      else { $this->{port} = $special_cfg{'p'}; }
+    if   ( $special_cfg{'p'} eq "" ) { $this->{port} = '1080'; }
+    else                             { $this->{port} = $special_cfg{'p'}; }
     $this->{sport} = 0;
-    $this->{vrfy} = "";
+    $this->{vrfy}  = "";
 
-    if (( $special_cfg{'u'} eq "" ) || ( $special_cfg{'v'} eq "" )){
-      &usage();
-      exit(1);
+    if ( ( $special_cfg{'u'} eq "" ) || ( $special_cfg{'v'} eq "" ) ) {
+        &usage();
+        exit(1);
     }
 
     $this->{username} = $special_cfg{'u'};
@@ -42,64 +42,65 @@ sub init{
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # how to quit ?
-sub getQuit{
-	return("");
+sub getQuit {
+    return ("");
 }
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # what to test without doing a login before
 # ..mainly the login stuff *g*
 sub getLoginarray {
-   my $this = shift;
-   @Loginarray = ("");
-   return (@Loginarray);
+    my $this = shift;
+    @Loginarray = ("");
+    return (@Loginarray);
 }
-
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # which commands does this protocol know ?
 sub getCommandarray {
-	my $this = shift;
+    my $this = shift;
 
-	# all there is to test is the username as far as it seems...
-	@cmdArray = (
-		"XAXAX\n",
-                # if the programmer is clever enough he always receives the packet
-                # in a buffer which is bigger than ~0x128 :)
-                "\x05\x01\x00\x04\xFF\x10",     # check for buffer access which should give a gpf
-                "\x05\x01\x00\x04\x50\x10"      # same here different value... lame :)
-                                
-	);
-	return(@cmdArray);
+    # all there is to test is the username as far as it seems...
+    @cmdArray = (
+        "XAXAX\n",
+
+        # if the programmer is clever enough he always receives the packet
+        # in a buffer which is bigger than ~0x128 :)
+        "\x05\x01\x00\x04\xFF\x10"
+        ,    # check for buffer access which should give a gpf
+        "\x05\x01\x00\x04\x50\x10"    # same here different value... lame :)
+
+    );
+    return (@cmdArray);
 }
-
-
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # what to send to login ?
-sub getLogin{ 		# login procedure
-	my $this = shift;
-        $count1 = char(length($this->{username}));
-        $count2 = char(length($this->{password}));
+sub getLogin {                        # login procedure
+    my $this = shift;
+    $count1 = char( length( $this->{username} ) );
+    $count2 = char( length( $this->{password} ) );
 
-	@login = (
-                #protocol version #nr. of authentication methods #username+password
-                "\x05\x01\x02",
-                #protocol #username len #username #pass len #password
-                "\x05$count1$this->{username}$count2$this->{password}";
-        );
-	return(@login);
+    @login = (
+
+        #protocol version #nr. of authentication methods #username+password
+        "\x05\x01\x02",
+
+        #protocol #username len #username #pass len #password
+        "\x05$count1$this->{username}$count2$this->{password}";
+    );
+    return (@login);
 }
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # here we can test everything besides buffer overflows and format strings
-sub testMisc{
-	my $this = shift;
-	return();
+sub testMisc {
+    my $this = shift;
+    return ();
 }
 
 sub usage {
-  print qq~ Parameters for the Socks5 plugin:
+    print qq~ Parameters for the Socks5 plugin:
  -u <username>
  -v <password>
 ~;
