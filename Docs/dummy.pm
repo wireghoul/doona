6dedfed43475952c3fc6b8618b33768b6e5f8a92
Eldar Marcussen <wireghoul@justanotherhacker.com>
2016-02-11 18:48:47 -0500

    Updating dummy.pm to a fix some typo's, references, etc. Also closes issue #6 and #7

--- Docs/dummy.pm
+++ Docs/dummy.pm
@@ -1,125 +1,113 @@
 package bedmod::dummy;
 use Socket;
 
-# Example plugin for bed2
-#
-# Feel free to fill in the gaps ... :)
-#
-# search bed.pl for "dummy" to see what you need to
-# change to include your module
-# (just one entry ... no need to be scared *g* )
+# Example plugin for a doona module
+# Replace the use of "dummy" with your module name
+# Copy the file to bedmod as <modulename.pm>
 #
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # create a new instance of this object
 sub new{
-	my $this = {};
+    my $this = {};
 
-	# define everything you might need
-	$this->{dummy}         = undef;
-	bless $this;
-	return $this;
+    # define everything you might need
+    $this->{something} = undef;
+    bless $this;
+    return $this;
 }
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # initialise some parameters
 sub init{
-	my $this = shift;
-	%special_cfg=@_;
+    my $this = shift;
+    %special_cfg=@_;
 
     # Set protocol tcp/udp
-    $this->{proto} = "tcp"; 
+    $this->{proto} = "tcp";
 
     # insert your default port here...
     if ($special_cfg{'p'} eq "") { $this->{port}='110'; }
       else { $this->{port} = $special_cfg{'p'}; }
 
-	# verify you got everything you need,
-	# $special_cfg will provide you the commandline
-	# switches from u, v, w and x
-	if ( $special_cfg{'u'} eq "") {
-		 print qq~
- 	Parameters for the dummy plugin:
+    # verify you got everything you need,
+    # $special_cfg will provide you the commandline
+    # switches from u, v, w and x
+    if ( $special_cfg{'u'} eq "") {
+        &usage;
+    }
 
- 		-u <description what the user should provide>
+    # set info necessary for for your module..
+    $this->{u} = $special_cfg{'u'};
 
-~;
-		 exit(1);
-	}
-
-	# set info nessecairy for for your module..
-	$this->{dummy} = $special_cfg{'u'};
-
-	# how can bed check that the server is still alive
-	# This string will simply be send to the server
-	# ( server should reply something to this )
-	$this->{vrfy} = "HELP\r\n";
+    # check that the server is still alive
+    die "Server failed health check!\n" unless($this->health_check());
 }
 
 # Perform a common action such as authenticating here
 # if it this check assume it has crashed
 sub health_check {
+    # Should send/receive packet and match expected behaviour to be considered healthy
+    # return true to continue fuzzing
+    return 1;
 }
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # how to quit ?
 sub getQuit{
-	# what to send to close the connection the right way
-	return("QUIT\r\n");
+    # what to send to close the connection the right way
+    return("QUIT\r\n");
 }
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-# what to test without doing a login before
-# ..mainly the login stuff *g*
+# what to test without authenticating
+# Typically the login stuff
 sub getLoginarray {
- my $this = shift;
- @Loginarray = (
- 	"USER XAXAX\r\n",
- 	"USER $this->{username}\r\nPASS XAXAX\r\n"
- )
- return (@Loginarray);
+    my $this = shift;
+    @Loginarray = (
+        "USER XAXAX\r\n",
+        "USER $this->{username}\r\nPASS XAXAX\r\n"
+    );
+    return (@Loginarray);
 }
 
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # which commands does this protocol know ?
 sub getCommandarray {
-	my $this = shift;
-	# the XAXAX will be replaced with the buffer overflow / format string data
-	# place every command in this array you want to test
-	@cmdArray = (
-		"foo XAXAX\r\n",
-		"bar XAXAX\r\n",
-		"XAXAX\r\n"
-	);
-	return(@cmdArray);
+    my $this = shift;
+    # the XAXAX will be replaced with the buffer overflow / format string data
+    # place every command in this array you want to test
+    @cmdArray = (
+        "foo XAXAX\r\n",
+        "bar XAXAX\r\n",
+        "XAXAX\r\n"
+    );
+    return(@cmdArray);
 }
 
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # what to send to login ?
-sub getLogin{ 		# login procedure
-	my $this = shift;
-	@login = (
-		"Hi, I am a dummy\r\n",
-		"This is my pass: foobar\r\n"
-	);
-	return(@login);
+sub getLogin{    # login procedure
+    my $this = shift;
+    @login = (
+        "Hi, I am a dummy\r\n",
+        "This is my pass: foobar\r\n"
+    );
+    return(@login);
 }
 
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # here we can test everything besides buffer overflows and format strings
 sub testMisc{
-	# Insert your favourite directory traversal bug here :)
-	my $this = shift;
-	return();
+    # Insert your favourite directory traversal bug here :)
+    my $this = shift;
+    return();
 }
 
 # Module specific help goes here
 # Leave an empty sub if there is no module specific help
 sub usage {
-  print qq~ Module spacific help:
+    print qq~
+  Parameters for the dummy plugin:
+
+    -u <description what the user should provide>
 ~;
+exit(1);
 }
 
 1;
